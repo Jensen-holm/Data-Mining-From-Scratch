1bcbb0b61dfb1b4a00bd459fcc590f6679e11ecf
Jensen-holm <holmj@mail.gvsu.edu>
2023-05-09 20:16:25 -0400

    changed favicon

--- src/pages/kmeans-results.tsx
+++ src/pages/kmeans-results.tsx
@@ -13,7 +13,7 @@ export default function Kmeans() {
                 <title>Jensen: Data-Miner;</title>
                 <meta name="description" content="Machine Learning Models From Scratch" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/gvsu.png" />
             </Head>
             <div className="relative">
                 <NavBar />
